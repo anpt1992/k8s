786190e9458728c1c060cb47ea6e167101040a3b
anpt1992 <anpt2009@outlook.com>
2025-08-13 20:00:05 +0700

    Apply database migrations at startup and update Kubernetes deployment files to include namespace

--- app/CrudApp/Program.cs
+++ app/CrudApp/Program.cs
@@ -29,4 +29,12 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+
+// Apply migrations at startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
